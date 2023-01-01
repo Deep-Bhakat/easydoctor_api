6c19eaf15bc1788d7a63eebc0e1d103286214910
Deep Bhakat <deepbhakat234@gmail.com>
2023-01-01 17:00:50 +0530

    modified

--- utils/database.js
+++ utils/database.js
@@ -4,6 +4,8 @@ var con = mysql.createConnection({
   host: "MYSQL5039.site4now.net",
   user: "a926dd_doctor",
   password: "Apiece_1000",
-  database: "db_a926dd_doctor"
+  database: "db_a926dd_doctor",
 });
+
+con.connect();
 module.exports = con;
