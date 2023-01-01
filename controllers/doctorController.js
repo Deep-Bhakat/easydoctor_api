9c83ff03061dd0785d3333bb48ee52eb2fbde5e8
Deep Bhakat <deepbhakat234@gmail.com>
2023-01-01 17:00:53 +0530

    modified

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,12 +1,12 @@
 
 const catchAsyncError = require('../utils/catchAsyncErrors');
 const ErrorHandler = require('../utils/errorHandler');
-const con = require('../utils/database')
+const connection = require('../utils/database')
 
 exports.checkPhone = catchAsyncError(async (req,res,next) => {
     const {phone} = req.params;
 
-    con.query(`SELECT * FROM employee_master WHERE emp_cnct=${phone} and job_role='doctor'`, function (err, result, fields) {
+    connection.query(`SELECT * FROM employee_master WHERE emp_cnct=${phone} and job_role='doctor'`, function (err, result, fields) {
         console.log(err);
         if(err){
             return next(new ErrorHandler(err.message, 500));
